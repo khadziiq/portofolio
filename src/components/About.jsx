123afcade19378d61f233f8e8349e3c950452c12
khoirulkahf <fahrulista@gmail.com>
2022-03-29 08:18:00 +0700

    new baru

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
           
           <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
             <div className="sm:text-right text-4xl font-bold ">
-              <p>Hi. I'm Fahrul, nice to meet you. Please take a look around</p>
+              <p>Hi. I'm Fahrul, nice to meet you.</p>
             </div>
             <div>
               <p>
